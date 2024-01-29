96b8e8769a3e7860af08b89202985fa8aa28fcae
as247 <as247@vui360.com>
2024-01-29 23:42:25 +0700

    Update backup / restore script

--- backup.sh
+++ backup.sh
@@ -14,13 +14,20 @@ if [ ! -f mysql.env ]; then
 fi
 root_password=""
 source ./env.sh
-#cd to backup.sh dir
-cd "$(dirname "$0")"
 mkdir -p backup
-backup_file="backup/$(date +%Y%m%d_%H%M%S)-$MYSQL_DATABASE.sql"
+#check backup file from argument
+if [ -z "$1" ]; then
+  backup_file="backup/$(date +%Y%m%d_%H%M%S)-$MYSQL_DATABASE.sql.gz"
+else
+  backup_file="$1"
+  #if file name not end with .gz then add it
+  if [[ "$backup_file" != *.gz ]]; then
+    backup_file="$backup_file.gz"
+  fi
+fi
 
-docker compose exec -T db mysqldump -u root -p"$root_password" --single-transaction --complete-insert "$MYSQL_DATABASE" > "$backup_file"
-gzip "$backup_file"
-echo "Backup saved to $backup_file.gz"
+
+docker compose exec -T db mysqldump -u root -p"$root_password" --single-transaction --complete-insert "$MYSQL_DATABASE" | gzip -c > "$backup_file"
+echo "Backup saved to $backup_file"
 #clean old backups
 find backup -type f -mtime +7 -name '*.gz' -delete
---
+++ restore.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#switch to bash if run by sh
+if [ -z "$BASH_VERSION" ]; then
+    exec bash "$0" "$@"
+fi
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Navigate to the script's directory
+cd "$SCRIPT_DIR" || exit
+
+if [ ! -f mysql.env ]; then
+  echo "mysql.env not found"
+  exit 1
+fi
+root_password=""
+source ./env.sh
+restore_file=""
+if [ -z "$1" ]; then
+  echo "Usage: restore.sh <backup_file>"
+  exit 1
+else
+  restore_file="$1"
+fi
+if [ ! -f "$restore_file" ]; then
+  echo "File $restore_file not found"
+  exit 1
+fi
+echo "Restore $restore_file"
+#drop all tables
+echo "Drop all tables"
+dropTables_file="mysql/helpers/droptables.sql"
+docker compose exec -T db mysql -u root -p"$root_password" "$MYSQL_DATABASE" < "$dropTables_file"
+echo "Restoring"
+#check if file is gzip
+if [[ "$restore_file" == *.gz ]]; then
+  gunzip -c "$restore_file" | docker compose exec -T db mysql -u root -p"$root_password" "$MYSQL_DATABASE"
+else
+  docker compose exec -T db mysql -u root -p"$root_password" "$MYSQL_DATABASE" < "$restore_file"
+fi
+echo "Restore done"
