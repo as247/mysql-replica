3ae8a9fdeab16140f6516ac3d91cbad95db91b06
alt <as247@vui360.com>
2025-07-20 15:14:17 +0700

    print phpmyadmin info

--- prepare.sh
+++ prepare.sh
@@ -200,8 +200,7 @@ echo "* Pass: $MYSQL_PASSWORD"
 echo "*********************************************************"
 if [ $PHPMYADMIN_PORT -ne 0 ]; then
   echo "PhpMyAdmin is enabled on port $PHPMYADMIN_PORT"
-  PUBLIC_IP=$(curl -s4 ifconfig.me)
-  echo "You can access it at http://$PUBLIC_IP:$PHPMYADMIN_PORT"
+  echo "You can access it at http://127.0.0.1:$PHPMYADMIN_PORT"
 else
   echo "PhpMyAdmin is disabled"
 fi
